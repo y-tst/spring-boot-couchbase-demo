7e95585763257219e50018c6eabfdd9c809cdfed
yurii_yanchuk <yurii_yanchuk@epam.com>
2023-07-23 22:01:42 +0300

    Initial commit

---
+++ src/main/java/edu/yanchuk/springbootcouchbasedemo/SpringBootCouchbaseDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.yanchuk.springbootcouchbasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCouchbaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCouchbaseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/yanchuk/springbootcouchbasedemo/SpringBootCouchbaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yanchuk.springbootcouchbasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCouchbaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
